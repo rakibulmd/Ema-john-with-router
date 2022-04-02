ee76d001494ded2baa922309dc73907142609392
Rakibul md <rakib245875@gmail.com>
2022-04-02 19:42:54 +0600

    added react router & some components

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Inventory.css";
+
+const Inventory = () => {
+    return (
+        <div>
+            <h2>THis is inventory</h2>
+        </div>
+    );
+};
+
+export default Inventory;
